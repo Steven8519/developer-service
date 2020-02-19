a93db934bf05d5b02a0ba9473cceeee8e928f706
stevenmcdonald <engineers2526@gmail.com>
2020-02-18 19:26:47 -0600

    test 2

--- src/main/java/info/devlink/developerservice/controller/DeveloperController.java
+++ src/main/java/info/devlink/developerservice/controller/DeveloperController.java
@@ -1,9 +1,31 @@
 package info.devlink.developerservice.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import info.devlink.developerservice.model.Developer;
+import info.devlink.developerservice.repository.DeveloperRepository;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping(path = "/developer")
+@RequestMapping(value = "/developers")
 public class DeveloperController {
+
+    private final DeveloperRepository developerRepository;
+
+    public DeveloperController(DeveloperRepository developerRepository) {
+        this.developerRepository = developerRepository;
+    }
+
+    @PostMapping()
+    public @ResponseBody
+    Mono<Developer> addKayak(@RequestBody Developer developer) {
+        return developerRepository.save(developer);
+    }
+
+    @GetMapping()
+    public @ResponseBody
+    Flux<Developer> getAllKayaks() {
+        Flux<Developer> result = developerRepository.findAll();
+        return result;
+    }
 }
